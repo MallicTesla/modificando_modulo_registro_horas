e074d26e63df488d900dd24239a5f6caa5bc9288
MallicTesla <el_mallic@hotmail.com>
2025-01-06 17:32:44 -0300

    primera modificacion

--- timesheet_billable/__manifest__.py
+++ timesheet_billable/__manifest__.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
     'name': "Horas Facturables",
 
@@ -11,14 +11,14 @@ poder agregar dos nuevos valores al registro de horas: coeficiente de
 facturación (porcentaje positivo) y horas facturables. 
     """,
 
-    'author': "Proyecta",
-    'website': "https://odoo.proyectasoft.com",
+    'author': "PrimateUY",
+    'website': "https://primate.uy/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'hr',
-    'version': '17.0.0.1',
+    'version': '17.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr_timesheet'],
--- timesheet_billable/models/analytic_account.py
+++ timesheet_billable/models/analytic_account.py
@@ -8,22 +8,25 @@ from odoo.osv import expression
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    billable_hours = fields.Float(string='Horas Facturables', default=0.0)
-
+    # Cambiar etiqueta del campo para reflejar el cambio a "Horas Dedicadas"
+    billable_hours = fields.Float(string='Horas Dedicadas', default=0.0)
 
     @api.model
     def create(self, vals):
+        # Sincronizar el valor de billable_hours al campo unit_amount (Horas Facturables)
         if vals.get('billable_hours', 0.0):
             if self.env.user.has_group('timesheet_billable.edit_billable_hours'):
                 billable_hours = vals.get('billable_hours', 0.0)
-                vals['billable_hours'] = billable_hours
+                vals['unit_amount'] = billable_hours  # Copiar valor a unit_amount
+
         if vals.get('unit_amount', False) and not vals.get('billable_hours', False):
             unit_amount = vals.get('unit_amount', 0.0)
-            vals['billable_hours'] = unit_amount
+            vals['billable_hours'] = unit_amount  # Sincronizar valores si no está definido billable_hours
+
         elif vals.get('unit_amount', False) and vals.get('billable_hours', False) and not self.env.user.has_group(
                 'timesheet_billable.edit_billable_hours'):
             unit_amount = vals.get('unit_amount', 0.0)
-            vals['billable_hours'] = unit_amount
+            vals['billable_hours'] = unit_amount  # Mantener consistencia de valores
 
         return super(AccountAnalyticLine, self).create(vals)
 
@@ -32,16 +35,17 @@ class AccountAnalyticLine(models.Model):
         if vals.get('billable_hours', 0.0):
             if user_can_change_billable_hours:
                 billable_hours = vals.get('billable_hours', 0.0)
-                vals['billable_hours'] = billable_hours
+                vals['unit_amount'] = billable_hours  # Copiar valor a unit_amount
             else:
-                vals['billable_hours'] = self.billable_hours
+                vals['billable_hours'] = self.billable_hours  # Preservar valor original si no tiene permiso
 
         if vals.get('unit_amount', False):
             unit_amount = vals.get('unit_amount', 0.0)
-            vals['billable_hours'] = unit_amount
+            vals['billable_hours'] = unit_amount  # Sincronizar valores
 
         res = super(AccountAnalyticLine, self).write(vals)
 
+        # Actualizar qty_delivered basado en billable_hours si es necesario
         if user_can_change_billable_hours and vals.get('billable_hours', 0.0) and self.so_line:
             to_sum = self.search_read([('so_line', '=', self.so_line.id)], fields=['billable_hours'])
             self.so_line.qty_delivered = sum(billable_hours['billable_hours'] for billable_hours in to_sum)
