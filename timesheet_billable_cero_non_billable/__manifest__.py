92e31958c47ca17a0071ced19304148df60baac3
MallicTesla <el_mallic@hotmail.com>
2025-01-27 12:02:12 -0300

    modelo timesheet_billable_cero_non_billable

---
+++ timesheet_billable_cero_non_billable/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cero horas facturable.",
+
+    'summary': """
+        Por defecto las horas facturables son cero si el proyecto es no facturable.""",
+
+    'description': """
+        Si el proyecto es no facturable, las horas facturables deben ser cero. 
+    """,
+
+    'author': "PrimateUY",
+    'website': "https://www.primate.uy",
+    'category': 'hr',
+    'version': '17.0.0.1',
+
+    'depends': ['base', 'sale_project', 'timesheet_billable'],
+
+}
